53d9edc0955e079798bb3ba747e3ce6fb640778f
VlaStoBIT <vladica.stojic@bit.admin.ch>
2025-09-11 21:43:27 +0200

    ✨ Bump version 2.2.0 (binary Swift package distribution)

--- Package.swift
+++ Package.swift
@@ -1,31 +1,28 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let version = "2.2.0"
+let checksum = "47cf4689b768411675e63f3cd5196c1dc71fa7b7755357da74df2f356b653cb1"
+
 let package = Package(
     name: "DidResolver",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "DidResolver",targets:["DidResolverSources"]),
+        .library(
+            name: "DidResolver",
+            targets: ["DidResolver", "DidResolverRemoteBinaryPackage"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "DidResolverWrapper",
-            dependencies:[
-                .target(name:"DidResolver", condition: .when(platforms: [.iOS]))
-            ],
-            path: "DidResolverWrapper"
+            name: "DidResolver"
         ),
         .binaryTarget(
-            name: "DidResolver",
-            path: "./didresolver.xcframework"
-        ),
-        .target(
-            name: "DidResolverSources",
-            dependencies: ["DidResolverWrapper"],
-            path: "swift-sources"
+            name: "DidResolverRemoteBinaryPackage",
+            url: "https://github.com/swiyu-admin-ch/didresolver-swift/releases/download/\(version)/didresolver-\(version).xcframework.zip",
+            checksum: "\(checksum)"
         )
     ]
 )
