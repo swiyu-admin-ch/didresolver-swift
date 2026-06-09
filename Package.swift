0350baf90bd2ddcbcdff576e6a56c48ee32bfd68
MarcelPfeuti <219968268+MarcelPfeuti@users.noreply.github.com>
2026-06-09 07:47:11 +0000

    :package: Automated commit made by the 'Build Swift language bindings' GitHub action workflow

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.8.0"
+let version = "2.8.1"
 let xcframework_name = "didresolver"
 let binary_target_url_github_owner = "swiyu-admin-ch"
 let binary_target_url_github_repo = "didresolver-swift"
-let checksum = "af9884d10afcbafbdcfc07a89e30349b0005faf72179e59745cdcfdc7f9a2f89"
+let checksum = "1acd1302fdc429be81583b0c496a6392759955fda3d998f0b18059913ded7d79"
 
 let package = Package(
     name: "DidResolver",
